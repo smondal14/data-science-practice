b42034b48ca54db5dbe5790fea0c477dd7475b73
smondal14 <136967193+smondal14@users.noreply.github.com>
2025-04-05 19:23:36 +0530

    Update 1.py
    Added basic data analysis

--- 1.py
+++ 1.py
@@ -3,3 +3,9 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
 df = pd.read_csv(url)
 print(df.head())
+print("\nDataset Info:")
+print(df.info())
+print("\nSummary Statistics:")
+print(df.describe())
+print("\nMissing Values:")
+print(df.isnull().sum())
